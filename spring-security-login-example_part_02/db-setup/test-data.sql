f5897c509188ffe1f7bfb94c0d86b63db9e800e5
tupacalypse187 <tupacalypse187@gmail.com>
2015-02-21 16:28:24 -0500

    no message

--- spring-security-login-example_part_02/db-setup/test-data.sql
+++ spring-security-login-example_part_02/db-setup/test-data.sql
@@ -1,5 +1,7 @@
-insert into USER_DETAILS values ('user','202cb962ac59075b964b07152d234b70'); -- password - 123
-insert into USER_DETAILS values ('admin','21232f297a57a5a743894a0e4a801fc3'); -- password - admin
+insert into USER_DETAILS values ('user','12dea96fec20593566ab75692c9949596833adc9'); -- password - 123
+insert into USER_DETAILS values ('admin','d033e22ae348aeb5660fc2140aec35850c4da997'); -- password - admin
+insert into USER_DETAILS values ('admin','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'); -- password - test
 
 insert into USER_AUTH values ('user', 'ROLE_USER');
 insert into USER_AUTH values ('admin', 'ROLE_ADMIN');
+insert into USER_AUTH values ('test', 'ROLE_ADMIN');

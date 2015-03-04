a16494ff0bb0273f22b2597f75688dcbeec7d2f6
tupacalypse187 <tupacalypse187@gmail.com>
2015-03-04 11:18:06 -0500

    no message

--- spring-adjudicator-002/script/mysql.sql
+++ spring-adjudicator-002/script/mysql.sql
@@ -1,27 +1,16 @@
 
-CREATE  TABLE users (
-  username VARCHAR(45) NOT NULL ,
-  password VARCHAR(45) NOT NULL ,
-  enabled TINYINT NOT NULL DEFAULT 1 ,
-  PRIMARY KEY (username));
+DROP DATABASE IF EXISTS spring-test;
+CREATE DATABASE spring-test;
+USE spring-test;
 
-CREATE TABLE user_roles (
-  user_role_id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(45) NOT NULL,
-  role varchar(45) NOT NULL,
-  PRIMARY KEY (user_role_id),
-  UNIQUE KEY uni_username_role (role,username),
-  KEY fk_username_idx (username),
-  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
 
-INSERT INTO users(username,password,enabled)
-VALUES ('adjudicator','123456', true);
-INSERT INTO users(username,password,enabled)
-VALUES ('alex','123456', true);
+CREATE TABLE `spring-test`.`userdetail` (
+  `tempid` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tempusername` VARCHAR(15) NOT NULL,
+  `temppassword` VARCHAR(40) NOT NULL,
+  `tempauthority` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`tempid`));
 
-INSERT INTO user_roles (username, role)
-VALUES ('adjudicator', 'ROLE_USER');
-INSERT INTO user_roles (username, role)
-VALUES ('adjudicator', 'ROLE_ADMIN');
-INSERT INTO user_roles (username, role)
-VALUES ('alex', 'ROLE_USER');
+INSERT INTO `spring-test`.`userdetail` (`tempusername`, `temppassword`, `tempauthority`) VALUES ('admin', 'admin', 'ROLE_ADMIN');
+INSERT INTO `spring-test`.`userdetail` (`tempusername`, `temppassword`, `tempauthority`) VALUES ('user', 'user', 'ROLE_USER');
+INSERT INTO `spring-test`.`userdetail` (`tempusername`, `temppassword`, `tempauthority`) VALUES ('test', 'test', 'ROLE_ADMIN');

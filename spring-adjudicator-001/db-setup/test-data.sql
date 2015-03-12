f959cbdf0ee2c86d6f17ce0fedbdfec7788223d7
tupacalypse187 <tupacalypse187@gmail.com>
2015-03-12 05:57:59 -0400

    Removed -001

--- spring-adjudicator-001/db-setup/test-data.sql
+++
@@ -1,7 +0,0 @@
-insert into USER_DETAILS values ('user','12dea96fec20593566ab75692c9949596833adc9'); -- password - 123
-insert into USER_DETAILS values ('admin','d033e22ae348aeb5660fc2140aec35850c4da997'); -- password - admin
-insert into USER_DETAILS values ('admin','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'); -- password - test
-
-insert into USER_AUTH values ('user', 'ROLE_USER');
-insert into USER_AUTH values ('admin', 'ROLE_ADMIN');
-insert into USER_AUTH values ('test', 'ROLE_ADMIN');
--- spring-adjudicator-001/src/main/webapp/404.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Error</title>
-</head>
-<body>
-    <h1> Sorry the system is not available right now. Please try again later.</h1>
-</body>
-</html>
